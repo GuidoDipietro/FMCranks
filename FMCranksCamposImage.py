7df098fee57e0f2d06bc35972a16e64cd168ccc6
Guido Dipietro <48221146+GuidoDipietro@users.noreply.github.com>
2019-03-14 18:33:46 -0300

    Update and rename main.py to FMCranksCamposImage.py

--- FMCranksCamposImage.py
+++ FMCranksCamposImage.py
@@ -6,15 +6,11 @@ Created on Thu Mar 14 17:11:20 2019
 """
 
 from src.list_to_image import list_2_image
-from src.FMCranksParserV3 import cleanData, getNames, getNumbers
+from src.FMCranksParser import cleanData, getNames, getNumbers
 
 def main():
     scores = getNumbers(cleanData("FMCranks.txt"))
-    print(scores)
     names = getNames(cleanData("FMCranks.txt"))
-    print(names)
-    #scores = random_scores()
-    #names = random_names()
     
     img = list_2_image(names, scores)
     img.save('image.png')    
